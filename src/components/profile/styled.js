4015d35e0f169ce648acdc920c3879f9c413df0c
Mateus Gonçalves Franco <mattfran67@gmail.com>
2021-11-27 21:15:05 -0300

    fixed Profile component on small screens

--- src/components/profile/styled.js
+++ src/components/profile/styled.js
@@ -2,7 +2,9 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
   display: flex;
+  justify-content: center;
   align-items: flex-start;
+  flex-wrap: wrap;
 `;
 
 export const WrapperInfoUser = styled.div`
